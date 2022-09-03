3044263513626a8800f756e634a2d363d0b80037
Thomas Schreiner <iteano.thom@hotmail.com>
2022-09-03 16:51:47 -0300

    feat: add login page request

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,37 @@
+import { Api } from "./models/api.js";
 import { Modal } from "./models/modal.js";
+
+class Login {
+    static async loginAccount() {
+        const inputs = document.querySelectorAll("form input")
+        for(let i of inputs) {
+            if(i.value == "") {
+                return true
+            }
+        }
+        
+        const body  = {
+            "email": inputs[0].value.toLowerCase(),
+            "password": inputs[1].value
+        }
+        await Api.loginUser(body, inputs)
+    }
+
+    static events() {
+        const form = document.querySelector("form")
+        form.addEventListener("submit", (event) => {
+            event.preventDefault()
+            Login.loginAccount()
+        })
+    }
+}
+
+Login.events()
+
+
 
 // setTimeout(() => {
 //     Modal.loginErro("Essa conta não existe, logo não pode acessar a página dos posts")
 // }, 1000)
+
+
--- src/scripts/models/api.js
+++ src/scripts/models/api.js
@@ -2,7 +2,7 @@ import { Modal } from "./modal.js"
 
 export class Api {
     static urlBase = "https://m2-rede-social.herokuapp.com/api/"
-    static token = localStorage.getItem("@kenzieRedeSocual:token")
+    static token = localStorage.getItem("@kenzieRedeSocial:token")
     static header = {
         "Content-Type": "application/json",
         Autorization: `token ${this.token}`
@@ -18,11 +18,32 @@ export class Api {
                                 .then(resp => {
                                     if(resp.email == "user with this email already exists.") {
                                         Modal.signupErro("Já existe outro usuário com este endereço de email.")
-                                        
+
                                     } else {
-                                        localStorage.setItem("@kenzieRedeSocual:token", resp.uuid)
                                         for(let i of inputs) { i.value = "" }
-                                        window.location.replace("../homePage/homePage.html")
+                                        window.location.replace("../../../index.html")
+                                    }
+                                    return resp
+                                })
+                                .catch(erro => console.log(erro))
+        return request
+    }
+
+    static async loginUser(body, inputs) {
+        const request = await fetch(`${this.urlBase}users/login/`, {
+                                    method: "POST",
+                                    headers: this.header,
+                                    body: JSON.stringify(body)
+                                })
+                                .then(resp => resp.json())
+                                .then(resp => {
+                                    if(resp.non_field_errors) {
+                                        Modal.signupErro("O email que você inseriu não está cadastrado, confira seu email ou cadastre-se")
+                                    } else {
+                                        for(let i of inputs) { i.value = "" }
+                                        localStorage.setItem("@kenzieRedeSocial:token", resp.token)
+                                        localStorage.setItem("@kenzieRedeSocial:user_uuid", resp.user_uuid)
+                                        window.location.replace("./src/pages/homePage/homePage.html")
                                     }
                                     return resp
                                 })
